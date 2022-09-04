5b8777d789038d9f6afcfa16a3fe1b84f1d64974
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-09-04 14:20:58 -0400

    - including new .js files

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'your_jwt_secret'; // this has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('./passport'); // local passport file
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, // this is the username you're encoding in the JWT
+        expiresIn: '7d', // this specifies expiration time
+        algorithm: 'HS256' // This algorithm is used to 'sign or encode values of JWT
+    });
+}
+
+// POST login
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false}, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, { session: false}, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
