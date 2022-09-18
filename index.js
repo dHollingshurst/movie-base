3607fab3a8e7946d5dcf46745c4fd1e2a990c8fc
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-09-18 09:27:04 -0400

    - temporary removal of authentication for /movies

--- index.js
+++ index.js
@@ -65,14 +65,14 @@ app.get('/', (req, res) => {
 })
 
 // send request for the ENTIRE movie list
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies", function (req, res) {
     Movies.find()
-        .then((movies) => {
+        .then(function (movies) {
             res.status(201).json(movies);
         })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
+        .catch(function (error) {
+            console.error(error);
+            res.status(500).send("Error: " + error);
         });
 });
 
