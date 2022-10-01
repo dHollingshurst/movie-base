3f59e55e425e2ef6e2de1160b99b6c6f13871c08
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-10-01 10:01:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,14 +65,14 @@ app.get('/', (req, res) => {
 })
 
 // send request for the ENTIRE movie list
-app.get("/movies", passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
-        .then(function (movies) {
+        .then((movies) => {
             res.status(201).json(movies);
         })
-        .catch(function (error) {
-            console.error(error);
-            res.status(500).send("Error: " + error);
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
         });
 });
 
