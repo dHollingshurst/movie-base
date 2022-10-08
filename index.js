ef3ae81144ee0bc4ba12175d00374639652e3a0a
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-10-08 11:26:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -246,6 +246,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     }
+
     let hashedPassword = Users.hashPassword(req.body.Password);
 
     Users.findOneAndUpdate({ Username: req.params.Username }, {
