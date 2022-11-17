a8166976f1cd1d2491c4cb42844a84741c023a2c
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-11-17 11:45:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use(express.static('public'));
 // READS
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 // send request for the ENTIRE movie list
